939f4a50158658f24b852b2224887d4cdaf09467
Duncan Donaldson <donaldsonduncan@gmail.com>
2011-05-31 18:22:22 -0700

    added calltables to makefile

--- src/calltables.c
+++ src/calltables.c
@@ -5,7 +5,7 @@ void* ip_protocol[MAX_PROTOCOLS];
 void setup_basic_ip_calls() {
   //icmp 1
   //igmp 2
-  ip_protocol[6] = (void*)print_tcp_packet;
+  ip_protocol+6 = (void*)print_tcp_packet;
   //udp 17
   //ipv6 29
   //routing header ipv6 43
